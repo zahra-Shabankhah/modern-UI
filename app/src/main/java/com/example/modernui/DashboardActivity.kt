698291e992cad6ab35f6717035f73350cf80c58e
Zahra Shabankhah <shabankhah1992@gmail.com>
2021-12-05 14:03:56 +0100

    5,December . Extension class

--- app/src/main/java/com/example/modernui/DashboardActivity.kt
+++ app/src/main/java/com/example/modernui/DashboardActivity.kt
@@ -1,25 +1,34 @@
 package com.example.modernui
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Toast
 import androidx.appcompat.widget.AppCompatImageButton
+import com.example.modernui.databinding.ActivityDashboardBinding
 import com.google.android.material.card.MaterialCardView
 
 class DashboardActivity : AppCompatActivity() {
+    lateinit var binding: ActivityDashboardBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_dashboard)
+        binding= ActivityDashboardBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+    /*    setContentView(R.layout.activity_dashboard)
         val backB : AppCompatImageButton = findViewById(R.id.backB)
         val logoutB : AppCompatImageButton = findViewById(R.id.logoutB)
-        val layoutCards : MaterialCardView = findViewById(R.id.layoutCards)
+        val layoutCards : MaterialCardView = findViewById(R.id.layoutCards)*/
+        showToast("my custom Toast with extention class")
 
-        backB.setOnClickListener {
+        binding.backB.setOnClickListener {
              Toast.makeText(this,"back Bottom",Toast.LENGTH_LONG).show()
          }
 
-        layoutCards.setOnClickListener {
+        binding.layoutCards.setOnClickListener {
             Toast.makeText(this,"${getString((R.string.layouts))} clicked.",Toast.LENGTH_LONG).show()
+            val intent = Intent(this,MainActivity::class.java)
+            startActivity(intent)
         }
 
     }
